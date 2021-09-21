ec20bf3c138ce8334cb6cb7636e965154dd2f5de
pierolenzo <pierolenzo@gmail.com>
2021-09-21 22:26:56 +0200

    Feat: create bind (parent -> child) between user-list and user-single

--- src/app/user-single/user-single.component.ts
+++ src/app/user-single/user-single.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from '../models/User'
 @Component({
   selector: 'app-user-single',
   templateUrl: './user-single.component.html',
   styleUrls: ['./user-single.component.sass']
 })
 export class UserSingleComponent implements OnInit {
+  @Input() item!: User;
 
   constructor() { }
 
