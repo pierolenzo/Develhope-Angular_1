b9486b65e8e7bc6fe12ccab168758ec786b98d77
pierolenzo <pierolenzo@gmail.com>
2021-09-21 14:29:35 +0200

    Feat: add models User, property user into user-detail.component

---
+++ src/app/models/User.ts
@@ -0,0 +1,39 @@
+export interface User {
+  id: number;
+  name: string;
+  surname: string;
+  age: number;
+  dateOfBirth: string;
+  address: Address;
+  role: Roles;
+  username: string;
+  profilePhotoUrl: string;
+  companies: Companies[];
+  gender: Gender;
+}
+
+export interface Address {
+  city: string;
+  street: string;
+  postalCode: string;
+}
+
+export interface Companies {
+  id: number;
+  name: string;
+  description: string;
+  location: Address;
+}
+
+export enum Roles {
+  staff = 'staff',
+  student = 'student',
+  manager = 'manager',
+  admin = 'admin',
+}
+
+export enum Gender {
+  male = 'male',
+  female = 'female',
+  other = 'other'
+}
--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Gender, Roles, User } from '../models/User';
 
 @Component({
   selector: 'app-user-detail',
@@ -6,6 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-detail.component.sass']
 })
 export class UserDetailComponent implements OnInit {
+  user: User = {
+    id: 3487,
+      name: 'Mario',
+      surname: 'Rossi',
+      age: 25,
+      dateOfBirth: '1995-14-12',
+      address: {
+          city: 'Roma',
+          street: 'Via roma 10',
+          postalCode: '00100'
+      },
+      role: Roles.staff,
+      username: 'MarioRossi80',
+      profilePhotoUrl: 'https://bit.ly/3yRngEP',
+      companies: [
+      {
+              id: 148979,
+              name: 'Develhope',
+              description: 'La migliore',
+              location: {
+                  city: 'Palermo',
+                  street: 'Via Libertà 58',
+                  postalCode: '90139'
+              }
+          },
+          {
+              id: 123123,
+              name: 'Apple',
+              description: 'E insomma...',
+              location: {
+                  city: 'Cupertino',
+                  street: 'One Apple Park Way',
+                  postalCode: '95014'
+              }
+          }
+    ],
+      gender: Gender.male
+  }
 
   constructor() { }
 
