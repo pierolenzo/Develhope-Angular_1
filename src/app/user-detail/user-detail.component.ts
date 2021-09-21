f7b11b98fac4c97de1d10195cf2e07620e9f75fa
pierolenzo <pierolenzo@gmail.com>
2021-09-21 21:11:39 +0200

    Feat: create interface user detail

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,54 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { KeyValue } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
 import { Gender, Roles, User } from '../models/User';
 
 @Component({
   selector: 'app-user-detail',
   templateUrl: './user-detail.component.html',
-  styleUrls: ['./user-detail.component.sass']
+  styleUrls: ['./user-detail.component.sass'],
 })
 export class UserDetailComponent implements OnInit {
   user: User = {
     id: 3487,
-      name: 'Mario',
-      surname: 'Rossi',
-      age: 25,
-      dateOfBirth: '1995-14-12',
-      address: {
-          city: 'Roma',
-          street: 'Via roma 10',
-          postalCode: '00100'
-      },
-      role: Roles.staff,
-      username: 'MarioRossi80',
-      profilePhotoUrl: 'https://bit.ly/3yRngEP',
-      companies: [
+    name: 'Mario',
+    surname: 'Rossi',
+    age: 25,
+    dateOfBirth: '1995-14-12',
+    address: {
+      city: 'Roma',
+      street: 'Via roma 10',
+      postalCode: '00100',
+    },
+    role: Roles.staff,
+    username: 'MarioRossi80',
+    profilePhotoUrl: 'https://bit.ly/3yRngEP',
+    companies: [
       {
-              id: 148979,
-              name: 'Develhope',
-              description: 'La migliore',
-              location: {
-                  city: 'Palermo',
-                  street: 'Via Libertà 58',
-                  postalCode: '90139'
-              }
-          },
-          {
-              id: 123123,
-              name: 'Apple',
-              description: 'E insomma...',
-              location: {
-                  city: 'Cupertino',
-                  street: 'One Apple Park Way',
-                  postalCode: '95014'
-              }
-          }
+        id: 148979,
+        name: 'Develhope',
+        description: 'La migliore',
+        location: {
+          city: 'Palermo',
+          street: 'Via Libertà 58',
+          postalCode: '90139',
+        },
+      },
+      {
+        id: 123123,
+        name: 'Apple',
+        description: 'E insomma...',
+        location: {
+          city: 'Cupertino',
+          street: 'One Apple Park Way',
+          postalCode: '95014',
+        },
+      },
     ],
-      gender: Gender.male
-  }
+    gender: Gender.male,
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
